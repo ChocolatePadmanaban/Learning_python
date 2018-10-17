c15c6de117a1738e0d875ebac4048d47d3cf8200
ChocolatePadmanaban <pradeeppadmanaban7@gmail.com>
2018-10-17 15:31:38 +0530

    third  commit classes

--- Day3/part3.py
+++ Day3/part3.py
@@ -2,11 +2,14 @@
 
 class Student:
     '''student class '''
+    total = 0
     def __init__(self, n,a):
         '''for privacy add __ before variable names'''
         self.__name = n
         self.__age= a
-        print("Hello")
+        self.__class__.total += 1
+        self.__roll = self.__class__.total
+        #print("Hello")
         '''we cannnat access without getters'''
 
     def get_age(self):
@@ -15,6 +18,8 @@ class Student:
         return self.__name
     def inc_age(self,n):
         self.__age += n
+    def get_roll(self):
+        return self.__roll
     
 
 
--- Day3/part4.py
+++ Day3/part4.py
@@ -3,10 +3,15 @@ from copy import deepcopy
 from part3 import Student
 stu1 = Student("Pradeep", 22)
 stu2 = Student("Ezhil", 32)
-stu3 = deepcopy(stu1)
+stu3 = deepcopy(stu1)# for deep constructor is not called
 print(stu1.get_name() + "'s age is", stu1.get_age())
 print(stu2.get_name() + "'s age is", stu2.get_age())
 stu1.inc_age(3)
+
 print(stu1.get_name() + "'s age is", stu1.get_age())
 print(stu2.get_name() + "'s age is", stu2.get_age())
 print(stu3.get_name() + "'s age is", stu3.get_age())
+
+print(stu1.get_name() + "'s roll no is", stu1.get_roll())
+print(stu2.get_name() + "'s roll no is", stu2.get_roll())
+print(stu3.get_name() + "'s roll no is", stu3.get_roll())
