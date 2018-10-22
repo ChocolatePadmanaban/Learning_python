e76de86a20e0a453574af173e20b1a8a391a2ca0
ChocolatePadmanaban <pradeeppadmanaban7@gmail.com>
2018-10-22 17:07:19 +0530

    Fourth day part 3

--- Day4/part7.py
+++ Day4/part7.py
@@ -10,7 +10,38 @@ def gen_a():
 
 
 print(func_a())
-print(next(gen_a()))
-print(next(gen_a()))
-print(next(gen_a()))
-print(next(gen_a()))
+a = gen_a()
+print(next(a))
+print(next(a))
+print(next(a))
+
+# Generators in file:
+
+def BeerData():
+    f = open('recipeData.csv')
+    for line in f:
+        yield line
+beer = BeerData()
+print(next(beer))
+print(next(beer))
+
+lines= (line for line in open('recipeData.csv'))
+print(next(lines))
+cols = (l.split(',') for l in lines)
+colums = next(cols)
+beerdicts = (dict(zip(colums,data)) for data in cols)
+print(next(beerdicts))
+
+
+
+# Difference between Function and Genrator
+
+lc = [n**2 for n in [1,2,3,4,5]]
+ge = (n**2 for n in [1,2,3,4,5])
+
+print(lc)
+print(next(ge))
+print(next(ge))
+print(next(ge))
+print(next(ge))
+print(next(ge))
