939d17e40c4e459dce898c4984502bd61fddc8dd
ChocolatePadmanaban <pradeeppadmanaban7@gmail.com>
2018-10-25 17:45:21 +0530

    incomplete

--- Day7/part_11.py
+++ Day7/part_11.py
@@ -1,7 +1,18 @@
 # milti dimenstional element finding(find the location of first occurance)
 
 
-
-
-
 A= [[1,2],[2,[5,6]],3]
+def Extract(a, e):
+    for i in a:
+        b = [a.index(i)]
+        if i == e:
+            print(i)
+            break
+        elif type(i) == list and Extract(i,e) != None:
+            c = Extract(i,e)
+            b = b + Extract(i,e)
+        else:
+            break
+    return b
+
+print(Extract(A, 1))
