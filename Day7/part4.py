c36e6f934b55bf1c992b3c153cf09bd79ccbdbbf
ChocolatePadmanaban <pradeeppadmanaban7@gmail.com>
2018-10-25 13:55:01 +0530

    day7 part 4 commit

--- Day7/part4.py
+++ Day7/part4.py
@@ -1,4 +1,6 @@
-# convert list to matrix
+#matrix operations:
+# 1.convert list to matrix
+# 2.MatrixTranspose
 
 def MatrixCreate(l,s):
     if s[0]*s[1] == len(l):
@@ -10,4 +12,18 @@ def MatrixCreate(l,s):
     else:
         return "Invalid Input"
 
+def MatrixTranspose(M):
+    m= len(M)
+    n = len(M[0])
+    answer = []
+    for i in range(n):
+        answer1 = []
+        for j in range(m):
+            answer1.append(M[j][i])
+        answer.append(answer1)
+    return answer
+
+
 print(MatrixCreate([11,-9,8,17,6,7],(3,2)))
+print(MatrixTranspose([[1,2,3],[4,5,6]]))
+
