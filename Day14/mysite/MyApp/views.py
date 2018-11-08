6ff2e3b7e4fee11d18eff85d4ebace0c084d23e0
ChocolatePadmanaban <pradeeppadmanaban7@gmail.com>
2018-11-08 12:23:04 +0530

    Django

---
+++ Day14/mysite/MyApp/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+import datetime
+
+def say_hello( name):
+    return HttpResponse("Hello " +name)
+def current_datetime(request):
+    now = datetime.datetime.now()
+    html = """<html><body> It is now%s               
+               </body></html>"""%now
+    return HttpResponse(html)
+
+def hours_ahead(request, offset):
+    offset = int(offset)
+    dt = datetime.datetime.now()+ datetime.timedelta(hours = offset)
+    html = """<html><body>In %s hours(s), it will be %s</body></html>
+    """%(offset,dt)
+    return HttpResponse(html)
