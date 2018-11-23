095de3056c10e193f881f6213eb2051b4d3a925f
ChocolatePadmanaban <pradeeppadmanaban7@gmail.com>
2018-11-23 14:02:30 +0530

    Last commit

---
+++ Day21/part4.py
@@ -0,0 +1,13 @@
+#
+
+class Student():
+    def Grade(self):
+        print('Hello')
+    def __del__(self):
+        print('I am dead')
+
+
+# if __name__ =='__main__':
+#     a = Student()
+#     cf = a.Grade()
+#     del a
